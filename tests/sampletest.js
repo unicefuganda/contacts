deee22ba5c5cac7b1dd4611a0afc793b17f8c263
James Oloo Onyango <jamesony@thoughtworks.com>
2014-09-08 17:11:46 +0300

    setup grunt-karma for testing

---
+++ tests/sampletest.js
@@ -0,0 +1,14 @@
+//<reference path="../source/app.js">
+
+describe("Sample tests", function () {
+
+		it("Add gives the correct result", function () {
+				var num1 = 1;
+				var num2 = 3;
+				var expected = 4;
+
+				var result = 4;
+
+				expect(result).toBe(expected);
+		});
+});
