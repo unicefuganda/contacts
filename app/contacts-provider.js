7d44917979b44a45931d133a09aa01ccb1d0a417
Tunji Sunmonu <tunji.sunmonu@gmail.com>
2014-09-16 10:41:56 +0300

    [Tunji] #4 refractored the contact-provider.js

--- app/contacts-provider.js
+++ app/contacts-provider.js
@@ -1,63 +1,64 @@
 var mongoose = require('mongoose');
 
 var contactSchema = mongoose.Schema({ firstname: String,
-                                      lastname: String,
-                                      phone: { type: String, unique: true },
-                                      createdOn: { type: Date, 'default': Date.now },
-                                      updatedOn: Date });
+    lastname: String,
+    phone: { type: String, unique: true },
+    createdOn: { type: Date, 'default': Date.now },
+    updatedOn: Date });
 
 var Contact = mongoose.model('Contact', contactSchema);
 
-var ContactsProvider = function(dbURI) {
-    if(mongoose.connection.readyState == 2) return;
+
+module.exports = function (dbURI) {
+    if (mongoose.connection.readyState == 2) return;
 
     var connectionURI = dbURI || 'mongodb://localhost/unicefcontacts';
-
     mongoose.connect(connectionURI);
-};
 
-ContactsProvider.prototype.add = function(contactDetails, callback) {
-  var contact = new Contact(contactDetails);
-  contact.save(function (err, savedContact) {
-    callback( err, savedContact);
-  });
-};
+    return {
+        add: function (contactDetails, callback) {
+            var contact = new Contact(contactDetails);
+            contact.save(function (err, savedContact) {
+                callback(err, savedContact);
+            });
+        },
 
-ContactsProvider.prototype.addAll = function(contacts, callback) {
-  Contact.create(contacts, function (err) {
-    callback();
-  });
-};
+        addAll: function (contacts, callback) {
+            Contact.create(contacts, function (err) {
+                callback();
+            });
+        },
 
-ContactsProvider.prototype.edit = function(contact_id, contactDetails, callback) {
-  Contact.findByIdAndUpdate(contact_id, { $set: contactDetails }, function (err, updatedContact) {
-    callback( err, updatedContact);
-  });
-};
+        edit: function (contact_id, contactDetails, callback) {
+            Contact.findByIdAndUpdate(contact_id, { $set: contactDetails }, function (err, updatedContact) {
+                callback(err, updatedContact);
+            });
+        },
 
-ContactsProvider.prototype.findAll = function(callback) {
-  Contact.find({ })
-      .select('firstname lastname phone')
-      .exec(function (err, contacts) {
-          callback( err, contacts);
-      });
-};
+        findAll: function (callback) {
+            Contact.find({ })
+                .select('firstname lastname phone')
+                .exec(function (err, contacts) {
+                    callback(err, contacts);
+                });
+        },
 
-ContactsProvider.prototype.find = function(matcher, callback) {
-  Contact.find( matcher, function (err, contacts) {
-    callback( err, contacts);
-  });
-};
+        find: function (matcher, callback) {
+            Contact.find(matcher, function (err, contacts) {
+                callback(err, contacts);
+            });
+        },
 
-ContactsProvider.prototype.findOne = function(matcher, callback) {
-  Contact.findOne( matcher, function (err, contact) {
-    callback( err, contact);
-  });
-};
+        findOne: function (matcher, callback) {
+            Contact.findOne(matcher, function (err, contact) {
+                callback(err, contact);
+            });
+        },
 
-ContactsProvider.prototype.deleteAll = function() {
-  Contact.remove( { }, function (err) {
-  });
-};
+        deleteAll: function () {
+            Contact.remove({ }, function (err) {
+            });
+        }
 
-module.exports = ContactsProvider;
+    }
+};
