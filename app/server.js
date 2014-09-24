fae848c78e233ae5a7cc1c14fbde574a7fdca0c9
wanderanimrod <wanderanimrod@gmail.com>
2014-09-24 21:56:45 +0300

    [nim] #X Adds CORS header to all responses coming from /api/*. TESTS NOT SEEING THE RESPONSES

--- app/server.js
+++ app/server.js
@@ -11,21 +11,25 @@ var port = process.env.PORT || 8005;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var allowCrossDomain = function(req, res, next) {
+var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Token, X-Username');
     next();
 };
 
-var logRequest = function(req, res, next) {
+var logRequest = function (req, res, next) {
     console.log('received request at ' + Date.now());
-    next();
 };
 
-router.use(allowCrossDomain);
+app.use("/api/*", function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'localhost');
+    next();
+});
 
-router.use(logRequest);
+//router.use(allowCrossDomain);
+
+//router.use(logRequest);
 
 router.get('/', ContactService.welcomeMessage);
 
@@ -40,7 +44,5 @@ router.put('/contacts/', ContactService.edit);
 app.use('/api', router);
 
 app.listen(port);
-
-console.log('Contacts service running ' + port);
 
 module.exports = app;
