cdd24b9b2ede5af7ea38d4555d92b832daa8ea57
Kelvin Smith <oneKelvinSmith@gmail.com>
2014-10-16 15:30:25 +0300

    [kelvin james] #60 add staging.sh

---
+++ scripts/staging.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "ssh into staging"
+ssh -t -t -i $HOME/.ssh/id_rsa_staging staging@196.0.26.51 << EOF
+echo "change to root user"
+sudo su
+echo "Go to home"
+cd /home
+echo "remove previous code"
+rm -rf contact-provisioning
+source /etc/profile
+echo "clone the provisoning repository"
+git clone https://github.com/unicefuganda/contact-provisioning.git
+echo "create chef directory"
+mkdir -p /home/contacts-staging/provisioning/chef/
+echo "copy cookbooks and roles to chef directory"
+cp -ar contact-provisioning/chef/cookbooks /home/contacts-staging/provisioning/chef/
+cp -ar contact-provisioning/chef/roles /home/contacts-staging/provisioning/chef/
+echo "add cookbook and roles path to solo.rb"
+rm /etc/chef/solo.rb
+echo "provision eums to staging"
+chef-solo -o role[all-in-one]
+exit
+exit
+EOF
