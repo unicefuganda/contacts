757c97d594437d47169aa0d29c6e3dd5b895e457
James Oloo Onyango <jamesony@thoughtworks.com>
2014-12-09 12:45:55 +0300

    [nim, james] #X parameterized staging script to work for any environment

--- scripts/staging.sh
+++ scripts/staging.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
+KEY_LOCATION=$1
+USER=$2
+ADDRESS=$3
+
 echo "ssh into staging"
-ssh -t -t -i $HOME/.ssh/id_rsa_staging staging@196.0.26.51 << EOF
+ssh -t -t -i ${KEY_LOCATION} ${USER}@${ADDRESS} << EOF
+
 echo "change to root user"
 sudo su
+
 echo "Go to home"
 cd /home
+
 echo "remove previous code"
 rm -rf contact-provisioning
 source /etc/profile
-echo "clone the provisoning repository"
-git clone https://github.com/unicefuganda/contact-provisioning.git
+
+echo "remove chef directory"
+rm -rf /home/contacts-staging/provisioning
+
 echo "create chef directory"
-mkdir -p /home/contacts-staging/provisioning/chef/
-echo "copy cookbooks and roles to chef directory"
-cp -ar contact-provisioning/chef/cookbooks /home/contacts-staging/provisioning/chef/
-cp -ar contact-provisioning/chef/roles /home/contacts-staging/provisioning/chef/
+mkdir -p /home/contacts-staging/provisioning
+
+echo "clone the provisoning repository"
+git clone https://github.com/unicefuganda/contact-provisioning.git /home/contacts-staging/provisioning
+
 echo "add cookbook and roles path to solo.rb"
 rm /etc/chef/solo.rb
+
 echo 'cookbook_path "/home/contacts-staging/provisioning/chef/cookbooks"' >> /etc/chef/solo.rb
+
 echo 'role_path "/home/contacts-staging/provisioning/chef/roles"' >> /etc/chef/solo.rb
+
 echo "provision contacts to staging"
 chef-solo -o role[all]
+
 echo "restart nginx"
 killall -9 nginx
 service nginx restart
