c730291fb364a7d7fffb18382e0be368cbd87611
Neil Coutinho <ncoutin@thoughtworks.com>
2015-04-02 12:40:00 +0300

    moving app structure to standard UNICEF structure.

--- scripts/staging.sh
+++ scripts/staging.sh
@@ -10,27 +10,27 @@ echo "change to root user"
 sudo su
 
 echo "Go to home"
-cd /home
+cd /var/www
 
 echo "remove previous code"
 rm -rf contact-provisioning
 source /etc/profile
 
 echo "remove chef directory"
-rm -rf /home/contacts-staging/provisioning
+rm -rf /var/www/contacts-staging/provisioning
 
 echo "create chef directory"
-mkdir -p /home/contacts-staging/provisioning
+mkdir -p /var/www/contacts-staging/provisioning
 
 echo "clone the provisoning repository"
-git clone https://github.com/unicefuganda/contact-provisioning.git /home/contacts-staging/provisioning
+git clone https://github.com/unicefuganda/contact-provisioning.git /var/www/contacts-staging/provisioning
 
 echo "add cookbook and roles path to solo.rb"
 rm /etc/chef/solo.rb
 
-echo 'cookbook_path "/home/contacts-staging/provisioning/chef/cookbooks"' >> /etc/chef/solo.rb
+echo 'cookbook_path "/var/www/contacts-staging/provisioning/chef/cookbooks"' >> /etc/chef/solo.rb
 
-echo 'role_path "/home/contacts-staging/provisioning/chef/roles"' >> /etc/chef/solo.rb
+echo 'role_path "/var/www/contacts-staging/provisioning/chef/roles"' >> /etc/chef/solo.rb
 
 echo "provision contacts to staging"
 chef-solo -o role[all]
