3c9f80b5e11ca203491565d180b0bc7a19d88f7b
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-06-26 16:03:35 +0300

    [#94162176] Fix contacts unit tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,5 +5,5 @@ set -e
 npm install
 npm install grunt-coveralls
 echo repo_token: $COVERALLS_REPO_TOKEN >> .coveralls.yml
-npm test
-grunt coveralls:grunt_coveralls_coverage
+PORT=8006 npm test
+PORT=8006 grunt coveralls:grunt_coveralls_coverage
