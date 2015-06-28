87c6464faf3d8ee9726c68acab983c1ee0345085
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-06-28 14:50:11 +0300

    [X] Temporarily disable push of coverage results to coverals due to HTTP 500 response

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,4 +6,4 @@ npm install
 npm install grunt-coveralls
 echo repo_token: $COVERALLS_REPO_TOKEN >> .coveralls.yml
 PORT=8006 npm test
-PORT=8006 grunt coveralls:grunt_coveralls_coverage
+#PORT=8006 grunt coveralls:grunt_coveralls_coverage
