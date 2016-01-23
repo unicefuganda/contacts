5db718556e683af27a022a1a51ded3ee67e99122
bfeng <fungbo@126.com>
2016-01-23 21:10:26 +0800

    [#X] add timeout logic

--- scripts/startContacts.sh
+++ scripts/startContacts.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-while [ -z "`netstat -tln | grep 27017`" ]; do
+while [ -z "`netstat -tln | grep -w 27017`" ]; do
   echo 'Waiting for MongoDB to start ...'
   sleep 1
+  if [ $((timeout+=1)) -eq 10 ]; then
+    break
+  fi
 done
 
 cd /opt/app/contacts
